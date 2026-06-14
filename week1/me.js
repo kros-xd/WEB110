7b004028d5d33fc90b092b9050c901d86a79af2c
Kris Lee <kdlee0401@gmail.com>
2026-06-13 20:12:10 -0500

    saving progress

--- week1/me.js
+++ week1/me.js
@@ -1,7 +1,7 @@
 // Kris Lee - 6/13/2026
 
 /*
-7 Variables I will use to describe myself:
+Step 2 -- 7 Variables I will use to describe myself:
     - 1.) const variable: NAME
         - Data type: string
         - Explanation: My NAME is a sequence of characters, which is stored
@@ -17,19 +17,44 @@
         - Data type: string
         - Explanation: 
 
-    - 4.) variable: name
+    - 4.) variable: favoriteAnimal
         - Data type: string
-        - Explanation: 
+        - Explanation: Another string, so far I'm keeping all my favorite categories
+            with a similar naming convention so it is consistent.
 
-    - 5.) variable: name
+    - 5.) variable: favoriteSeason
         - Data type: string
-        - Explanation: 
+        - Explanation: I think my favorite season is Fall, but I also like Spring.
+            This also needs to be a string.
 
-    - 6.) variable: name
-        - Data type: string
-        - Explanation: 
+    - 6.) variable: myAge
+        - Data type: number
+        - Explanation: This uses an number (integer) data type. It shouldn't be a constant
+            because it is prone to changing every year.
 
-    - 7.) variable: name
-        - Data type: string
-        - Explanation: 
+    - 7.) variable: isStudent
+        - Data type: boolean
+        - Explanation: This is a logical primitive data type. 'isStudent' is a boolean, 
+            because we only need to know if I'm a student or not (true || false).
 */
+
+// Step 3 -- Declare variables about myself:
+const NAME = "Kris";
+let favoriteColor = "Black Forest Green";
+let favoriteFood = "Korean BBQ";
+let favoriteAnimal = "Cats";
+let favoriteSeason = "Fall";
+let myAge = 23;
+let isStudent = true;
+
+// Step 4 -- Data type check:
+/*
+    Q: Which variable could easily have the wrong data type? 
+        What mistake might someone make with it?
+    
+    A: I think the variable 'myAge' is most likely to have the wrong data type.
+        Someone might use a string for their age, which could be harder to increment
+        or use calculations later in their program.
+*/
+
+// Step 5 -- Display facts using template literals
