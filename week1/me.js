0f51e216c6ff97409bf0b3bc23adabc2b28994e4
Kris Lee <kdlee0401@gmail.com>
2026-06-13 19:38:44 -0500

    Working on first week's assignment!

---
+++ week1/me.js
@@ -0,0 +1,35 @@
+// Kris Lee - 6/13/2026
+
+/*
+7 Variables I will use to describe myself:
+    - 1.) const variable: NAME
+        - Data type: string
+        - Explanation: My NAME is a sequence of characters, which is stored
+            in a constant variable, because I don't plan on changing my name.
+    
+    - 2.) variable: favoriteColor
+        - Data type: string
+        - Explanation: Also a sequence of characters and I considered making this
+            a constant variable as well, but my favorite color changes often so I
+            decided not to.
+
+    - 3.) variable: favoriteFood
+        - Data type: string
+        - Explanation: 
+
+    - 4.) variable: name
+        - Data type: string
+        - Explanation: 
+
+    - 5.) variable: name
+        - Data type: string
+        - Explanation: 
+
+    - 6.) variable: name
+        - Data type: string
+        - Explanation: 
+
+    - 7.) variable: name
+        - Data type: string
+        - Explanation: 
+*/
